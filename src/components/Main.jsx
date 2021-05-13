a87e731f1c3a7da7f22d907705fe9d6918b67881
eunsol0410 <eunsollee04@gmail.com>
2021-05-14 03:06:50 +0900

    feat(index): add index

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import '../css/main.css'
 import Button from "react-bootstrap/Button";
+import Footer from "./Footer";
+import {Link} from "react-router-dom";
 
 class Main extends Component {
     constructor(props) {
@@ -23,17 +25,20 @@ class Main extends Component {
                             루트코인으로 시작하기
                         </span>
                     </div>
-                    <div className="main_message_sub text-muted">
-                        금값처럼 오른 대파, 직접 재배해 보세요.<br/>
-                        가상포인트로 매수, 매매하면서 실시간으로 변하는 농산물의 가격을 체험해보세요.<br/>
+                    <div className="main_message_sub text-black-50">
+                        금값처럼 오른 대파, 직접 재배해 보세요. 포인트로 매수, 매도하면서 실시간으로 변하는 농산물의 가격을 체험해보세요.
                         루트코인을 따라 집에서 직접 농산물을 재배하며 소소한 기쁨을 느껴주세요.
                     </div>
                     <div className="main_message_button">
                         <Button className="btn-color-green1" size="sm" active>
-                            실시간 시세 보기
+                            <Link to={"/trade"}>
+                                실시간 시세 보기
+                            </Link>
                         </Button>{' '}
                         <Button variant="light" size="sm" active>
-                            재배하기
+                            <Link to={"/grow"}>
+                                재배하기
+                            </Link>
                         </Button>
                     </div>
                 </div>
