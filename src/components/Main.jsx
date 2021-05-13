33aaaeb093bde341c0abc391fbf5362d8cedb8cc
SongKiYeon <proxima4311@naver.com>
2021-05-13 16:29:45 +0900

    landing Page Main Component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../css/main.css'
 
 class Main extends Component {
     constructor(props) {
@@ -7,8 +8,16 @@ class Main extends Component {
     }
     render() {
         return (
-            <div>
-            </div>
+            <main className="swm_main">
+                <div className="main_img">
+                    <img src="./imgs/main_back.jpg"/>
+                </div>
+                <div className="main_message">
+                    <div className="main_message_header">자연 친화적인 화폐, 루트코인</div>
+                    <div className="main_message_sub">농업을 장려하고 보상을 제공합니다</div>
+                    <div className="main_message_logo">RootCoin</div>
+                </div>
+            </main>
         );
     }
 }
