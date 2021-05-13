cdfcbac8ad0d0ae93d46d71a95006afec3f95536
eunsol0410 <eunsollee04@gmail.com>
2021-05-14 01:09:20 +0900

    feat(index): renew index (WIP)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../css/main.css'
+import Button from "react-bootstrap/Button";
 
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
+
     render() {
         return (
             <main className="swm_main">
-                <div className="main_img">
-                    <img src="./imgs/main_back.jpg"/>
-                </div>
+                {/*<div className="main_img">*/}
+                {/*    <img src="./imgs/main_back.jpg"/>*/}
+                {/*</div>*/}
                 <div className="main_message">
-                    <div className="main_message_header">자연 친화적인 화폐, 루트코인</div>
-                    <div className="main_message_sub">농업을 장려하고 보상을 제공합니다</div>
-                    <div className="main_message_logo">RootCoin</div>
+                    <div className="main_message_header">
+                        <span className="color-green1">
+                            첫 파테크,<br/>
+                        </span>
+                        <span className="color-green2">
+                            루트코인으로 시작하기
+                        </span>
+                    </div>
+                    <div className="main_message_sub text-muted">
+                        금값처럼 오른 대파, 직접 재배해 보세요.<br/>
+                        가상포인트로 매수, 매매하면서 실시간으로 변하는 농산물의 가격을 체험해보세요.<br/>
+                        루트코인을 따라 집에서 직접 농산물을 재배하며 소소한 기쁨을 느껴주세요.
+                    </div>
+                    <div className="main_message_button">
+                        <Button className="btn-color-green1" size="sm" active>
+                            실시간 시세 보기
+                        </Button>{' '}
+                        <Button variant="light" size="sm" active>
+                            재배하기
+                        </Button>
+                    </div>
                 </div>
             </main>
         );
     }
 }
+
 export default Main;
