97fc2a7bc0ed0d849e3b7315ee8be4c8bc0d9113
unknown <66f@naver.com>
2021-05-13 22:30:38 +0900

    Edit Category/Ranking Part

--- src/components/Trade/TradeSideBar.jsx
+++ src/components/Trade/TradeSideBar.jsx
@@ -4,11 +4,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, NavbarBrand } from 'react-bootstrap';
 
 class SideBar extends Component {
+    constructor(props){
+        super(props);
+        this.toggleCategoryOrRanking=this.toggleCategoryOrRanking.bind(this);        
+        this.state={
+            categoryOrRanking : 1
+        }
+    }
+    toggleCategoryOrRanking(choice){
+        console.log(choice);    
+        this.setState(()=>{
+            return {categoryOrRanking: choice};
+        });
+    }
     render() {
+        if(this.state.categoryOrRanking == 1)
         return (
             <div className="trade_sidebar">
-                <SideBarHeader/>
+                <SideBarHeader 
+                toggleCategoryOrRanking={this.toggleCategoryOrRanking}/>
                 <SideBarCategoryContent/>
+            </div>
+        );
+        else
+        return (
+            <div className="trade_sidebar">
+                <SideBarHeader
+                toggleCategoryOrRanking={this.toggleCategoryOrRanking}/>
+                <SideBarRankingContent/>
             </div>
         );
     }
@@ -24,9 +47,11 @@ class SideBarHeader extends Component{
                             switch (selectedKey) {
                                 case "category":
                                     console.log(this.props);
+                                    this.props.toggleCategoryOrRanking(1);
                                     break;
                                 case "ranking":
                                     console.log(this.props);
+                                    this.props.toggleCategoryOrRanking(2);
                                     break;
                             }
                         }}
@@ -44,10 +69,26 @@ class SideBarHeader extends Component{
     }
 }
 
+class SideBarCategoryItem extends Component{
+    render(){
+        return (
+            <div className="trade_sidebar_category_item">
+                <span>{this.props.coinName}</span>
+                <span>{this.props.price}</span>
+            </div>
+        );
+    };
+}
+
 class SideBarCategoryContent extends Component{
     render(){
         return(
-            null
+            <div className="trade_sidebar_category_content">
+                <SideBarCategoryItem coinName="감자" price="830"/>
+                <SideBarCategoryItem coinName="고구마" price="920"/>
+                <SideBarCategoryItem coinName="토마토" price="220"/>
+                <SideBarCategoryItem coinName="쪽파" price="1550"/>
+            </div>
         );
     }
 }
@@ -56,7 +97,9 @@ class SideBarCategoryContent extends Component{
 class SideBarRankingContent extends Component{
     render(){
         return(
-            null
+            <div className="trade_sidebar_ranking_content">
+                fdsa
+            </div>
         );
     }
 }
